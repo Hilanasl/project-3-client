4a41efd1c5ac75af6c20b07e3c25a82ecbd35976
Alix Virot <flying.cat@mac.com>
2022-02-09 13:11:37 +0100

    Work on Forms

--- src/Components/Forms/FormActivity.jsx
+++ src/Components/Forms/FormActivity.jsx
@@ -1,44 +1,57 @@
-import React from 'react';
+import React from "react";
 
-const FormActivity = (props) => {
-  const {activityTitle, setActivityTitle, address, setAddress, activityDescription, setActivityDescription } = props;
-
-  // return <div>todo !</div>
+const FormActivity = ({
+  // callback,
+  activity,
+  setDays,
+  activityNumber,
+  dayNumber,
+}) => {
+  const activityChange = (e) => {
+    setDays((prevState) => {
+      const newState = [...prevState];
+      //    Global state [Day X]  [Activity X]  {title....}
+      newState[dayNumber][activityNumber] = {
+        ...newState[dayNumber][activityNumber],
+        [e.target.name]: e.target.value,
+      };
+      return newState;
+    });
+  };
   return (
     <>
-    <div>
-    <label htmlFor="activityTitle">Title</label>
+      <div>
+        <label htmlFor={"activityTitle-" + activityNumber}>Title</label>
         <input
-            type="text"
-            id="activityTitle"
-            name="activityTitle"
-            value={activityTitle}
-            onChange={(e) => setActivityTitle(e.target.value)}
+          type="text"
+          name="title" // should I let it ? bc that's not what I was submitting to back. Same as TripForm
+          id={`activityTitle-${activityNumber}`}
+          value={activity.title}
+          onChange={activityChange}
         />
-  </div>
-  <div>
-    <label htmlFor="address">Address</label>
+      </div>
+      <div>
+        <label htmlFor={"address-" + activityNumber}>Address</label>
         <input
-            type="text"
-            id="address"
-            name="address"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
+          type="text"
+          name="address"
+          value={activity.address}
+          onChange={activityChange}
         />
-  </div>
-  <div>
-    <label htmlFor="activityDescription">Description</label>
+      </div>
+      <div>
+        <label htmlFor={"activityDescription-" + activityNumber}>
+          Description
+        </label>
         <input
-            type="text"
-            id="activityDescription"
-            name="activityDescription"
-            value={activityDescription}
-            onChange={(e) => setActivityDescription(e.target.value)}
+          type="text"
+          name="description"
+          value={activity.description}
+          onChange={activityChange}
         />
-  </div>
-
-  </>
-  )
+      </div>
+    </>
+  );
 };
 
 export default FormActivity;
--- src/Components/Forms/FormDay.jsx
+++ src/Components/Forms/FormDay.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
-import FormActivity from './FormActivity';
+import React, { useState } from "react";
+import FormActivity from "./FormActivity";
 
-const FormDay = (props) => {
+const FormDay = ({ dayNumber, activities, setDays }) => {
+  console.log("It's a brand new day !");
+
+  // ADD AN ACTIVITY
+  const addActivity = (event) => {
+    event.preventDefault();
+    setDays((prevState) => {
+      const newState = [...prevState];
+      newState[dayNumber] = [
+        ...newState[dayNumber],
+        { title: "", address: "", description: "" },
+      ];
+      return newState;
+    });
+  };
+  console.log("Activities :", activities);
+  // console.log("Title :", title);
+  // console.log("Address :", address);
+  // console.log("Description :", description);
+
+  if (!activities) return null;
   return (
     <div>
-    <h2>Day n° {props.day}</h2>
-    <FormActivity props={props}/>
+      <h2>Day n° {dayNumber + 1}</h2>
+
+      <button onClick={addActivity}>Add an activity</button>
+
+      <p>Number of activities {activities.length} </p>
+      {activities.map((activity, i) => (
+        <FormActivity
+          // callback={callback}
+          activity={activity}
+          dayNumber={dayNumber}
+          activityNumber={i}
+          setDays={setDays}
+          key={i}
+        />
+      ))}
     </div>
-  )
+  );
 };
 
 export default FormDay;
@@ -15,5 +48,4 @@ export default FormDay;
 // A new trip = > 1 or several days
 // Each day => store 1 or several activities
 // Trip has days
-// Days has activities 
-
+// Days has activities
